801f2b3f3386c15712179ee8cd6b3b098adb9937
Ken Saggy <ken@panoply.io>
2016-04-05 16:41:39 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# panoply-couchbase-nodejs
-A simple wrapper around the Couchbase NodeJS SDK that compiles it with SSL Support (linux only)
+# Couchbase NodeJS SDK Wrapper
+This is a thin and simple wrapper around the Couchbase SDK for NodeJS.
+On linux machines it will run a pre-install script to compile a separate version of libcouchbase with SSL support (which has been disabled in the default couchbase package)
+then install the default package with the `--couchbase-root` argument set to the correct location so that the bindings compile correctly.
---
+++ bin/preinstall.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Set to exit on error
+set -e
+
+CWD=$(pwd)
+INSTALLATION_DIR="$CWD/deps/libcouchbase/inst/"
+UNAME=$(uname)
+if [[ $UNAME != "Linux" ]]; then
+    echo "Only install Couchbase with SSL support on Linux"
+    echo "Default to using regular version of couchbase"
+    exit 0
+fi
+
+# Test existance of needed utils
+REQUIRED=( "git" "cmake" )
+for cmd in "${REQUIRED[@]}"; do
+    command -v $cmd >/dev/null 2>&1 || { echo >&2 "panoply-couchbase requires $cmd to build but it's not installed. Aborting."; exit 1; }
+done
+
+if [[ ! -d "$CWD/deps/libcouchbase" ]]; then
+    mkdir -p "$CWD/deps"
+    echo "Cloning libcouchbase from github"
+    git clone git://github.com/couchbase/libcouchbase.git ./deps/libcouchbase >/dev/null 2>&1
+else
+    echo "libcouchbase directory already exists. Skipping git clone"
+fi
+
+if [[ ! -d "$CWD/deps/libcouchbase/build" ]]; then
+
+    if [[ ! -d "$INSTALLATION_DIR" ]]; then
+        echo "Creating installation directory"
+        mkdir -p $INSTALLATION_DIR
+    fi
+
+    echo "Running configure script"
+    cd ./deps/libcouchbase && ./configure.pl --prefix=$INSTALLATION_DIR >/dev/null 2>&1
+    echo "Running build"
+    (make && make install) >/dev/null 2>&1
+
+else
+    echo "Installaton directory already exists. Skipping build"
+fi
+
+# Return to root directory
+cd $CWD
+echo "Using couchbase root: $INSTALLATION_DIR"
+echo "Executing: 'npm install --build-from-source --couchbase-root=$INSTALLATION_DIR couchbase@2.1.3'"
+npm install --build-from-source --couchbase-root=$INSTALLATION_DIR couchbase@2.1.3
